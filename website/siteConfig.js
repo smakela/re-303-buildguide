20797986bb13ba84d404ce05b89fb4f0f5d7a468
Sami Mäkelä <smakela@iki.fi>
2018-09-18 00:43:12 +0300

    remove opengraph and twitter images fron config

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -71,8 +71,8 @@ const siteConfig = {
   cleanUrl: true,
 
   // Open Graph and Twitter card images.
-  ogImage: 'img/docusaurus.png',
-  twitterImage: 'img/docusaurus.png',
+  ogImage: '',
+  twitterImage: '',
 
   // You may provide arbitrary config keys to be used as needed by your
   // template. For example, if you need your repo's URL...
