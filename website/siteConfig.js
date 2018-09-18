3379eb6767e9b9ec760c4964eefd849007b36889
Sami Mäkelä <smakela@iki.fi>
2018-09-18 12:08:21 +0300

    Update siteConfig.js

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -11,8 +11,8 @@
 const siteConfig = {
   title: 'RE-303 Build Guide', // Title for your website.
   tagline: 'Its not a clone, itss a replica',
-  url: 'http://smakela.github.io/re-303-buildguide/', // Your website URL
-  baseUrl: '/', // Base URL for your project */
+  url: 'http://smakela.github.io', // Your website URL
+  baseUrl: '/re-303-buildguide/', // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
   //   baseUrl: '/test-site/',
